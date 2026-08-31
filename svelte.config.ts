a0388f6ed9f9e37b39b1490df9e46d6729740dca
iInvisibilities <abuserod@gmail.com>
2026-08-31 14:00:18 +0100

    add telemetry api for redis

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-vercel'; // Ensure this points to Vercel
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  }
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter() // Explicitly targets Vercel's infrastructure
+    }
 };
 
 export default config;
